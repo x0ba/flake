a8726e88d9d0c6b2c8efbf0c2a0fa6507c2373f2
daniel <danielxu0307@proton.me>
2024-12-22 21:16:47 -0800

    add darwin configs

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -1,11 +1,12 @@
-{pkgs, ...}:
+{pkgs, inputs, lib, ...}:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
+  nativeBuildInputs = (with pkgs; [
     git
     alejandra
     just
     nix-output-monitor
     nixd
     nvd
-  ];
+  ])
+  ++ lib.optionals pkgs.stdenv.isDarwin [ inputs.darwin.packages.darwin-rebuild ];
 }
