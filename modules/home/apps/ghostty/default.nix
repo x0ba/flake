433dfed10d51a3888eca8e764786a46deb2a63f7
daniel <danielxu0307@proton.me>
2024-12-25 16:19:29 -0800

    change starship config

--- modules/home/apps/ghostty/default.nix
+++ modules/home/apps/ghostty/default.nix
@@ -26,7 +26,7 @@ in {
       enable = true;
       settings = {
         font-size = 13;
-        font-family = "JetBrainsMono Nerd Font";
+        font-family = "BerkeleyMono Nerd Font";
 
         theme = "catppuccin-mocha";
 
