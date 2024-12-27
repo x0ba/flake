88238870af0739f92ca83b9ab32a3f719e126e19
daniel <danielxu0307@proton.me>
2024-12-27 13:02:46 -0800

    format

--- modules/home/cli/zsh/default.nix
+++ modules/home/cli/zsh/default.nix
@@ -6,11 +6,20 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
+  inherit (pkgs.stdenv) isLinux;
   zshPlugins = plugins: (map (plugin: rec {
       name = src.name;
       inherit (plugin) file src;
     })
     plugins);
+
+  linuxOpen =
+    # bash
+    ''
+      function open() {
+        nohup xdg-open "$*" > /dev/null 2>&1
+      }
+    '';
 
   cfg = config.${namespace}.cli.zsh;
 in {
@@ -24,7 +33,59 @@ in {
       autosuggestion.enable = true;
       enableCompletion = true;
       dotDir = ".config/zsh";
+      initExtraFirst =
+        # bash
+        ''
+          zvm_config() {
+            ZVM_INIT_MODE=sourcing
+            ZVM_CURSOR_STYLE_ENABLED=false
+            ZVM_VI_HIGHLIGHT_BACKGROUND=black
+            ZVM_VI_HIGHLIGHT_EXTRASTYLE=bold,underline
+            ZVM_VI_HIGHLIGHT_FOREGROUND=white
+          }
+        '';
+      initExtra =
+        # bash
+        ''
+          function incognito() {
+            if [[ -n $ZSH_INCOGNITO ]]; then
+              add-zsh-hook precmd _atuin_precmd
+              add-zsh-hook preexec _atuin_preexec
+              unset ZSH_INCOGNITO
+            else
+              add-zsh-hook -d precmd _atuin_precmd
+              add-zsh-hook -d preexec _atuin_preexec
+              export ZSH_INCOGNITO=1
+            fi
+          }
+
+          onefetch_in_git_dir() {
+            if [[ -d '.git' ]]; then
+              ${pkgs.onefetch}/bin/onefetch --no-merges --no-bots --no-color-palette --true-color=never --text-colors 1 1 3 4 4
+            fi
+          }
+
+          add-zsh-hook chpwd onefetch_in_git_dir
+
+          ${lib.optionalString isLinux linuxOpen}
+        '';
+      oh-my-zsh = {
+        enable = true;
+        plugins =
+          [
+            "colored-man-pages"
+            "colorize"
+            "git"
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
+            "macos"
+          ];
+      };
       plugins = zshPlugins [
+        {
+          src = pkgs.zsh-vi-mode;
+          file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+        }
         {
           src = pkgs.zsh-nix-shell;
           file = "share/zsh-nix-shell/nix-shell.plugin.zsh";
@@ -32,10 +93,6 @@ in {
         {
           src = pkgs.zsh-fast-syntax-highlighting;
           file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
-        }
-        {
-          src = pkgs.zsh-fzf-tab;
-          file = "share/fzf-tab/fzf-tab.plugin.zsh";
         }
       ];
       shellAliases = {
@@ -50,18 +107,6 @@ in {
         mv = "mv -i";
         rm = "rm -i";
       };
-      initExtra =
-        # bash
-        ''
-          # set emacs keybinds
-          bindkey -e
-
-          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-          zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-          zstyle ':completion:*' menu no
-          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-          zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
-        '';
       history.path = "${config.xdg.configHome}/zsh/history";
     };
   };
