d5ed0e5d8d7721a9612e9ee57fefddc3e451175a
daniel <danielxu0307@proton.me>
2024-12-27 18:45:39 -0800

    noice

--- modules/home/cli/utils/default.nix
+++ modules/home/cli/utils/default.nix
@@ -96,7 +96,116 @@ in {
       };
       starship = {
         enable = true;
-        settings = builtins.fromTOML (builtins.readFile ./starship/config.toml);
+        settings = {
+          command_timeout = 3000;
+          format = "$hostname$username$nix_shell$character";
+          right_format = "$directory\${custom.jjid}\${custom.jjstat}$git_state$git_commit$git_status";
+
+          character = {
+            success_symbol = "[ ♥ ](fg:black bg:cyan)";
+            error_symbol = "[ ♥ ](fg:black bg:red)";
+            vimcmd_symbol = "[ ♡ ](fg:black bg:purple)";
+            vimcmd_replace_symbol = "[ ♡ ](fg:black bg:green)";
+            vimcmd_replace_one_symbol = "[ ♡ ](fg:black bg:green)";
+            vimcmd_visual_symbol = "[ ♡ ](fg:black bg:yellow)";
+          };
+
+          username = {
+            style_user = "bg:purple fg:black";
+            style_root = "bg:red fg:black";
+            format = "[ $user ]($style)";
+            disabled = false;
+            show_always = true;
+          };
+
+          hostname = {
+            style = "fg:black bg:blue";
+            ssh_only = true;
+            ssh_symbol = "";
+            format = "[ ✦ $hostname ✦ ]($style)";
+            disabled = false;
+          };
+
+          git_commit = {
+            style = "fg:black bg:purple";
+            format = ''[ $hash$tag ]($style)'';
+          };
+
+          git_state = {
+            style = "fg:black bg:red";
+            format = "[ $state $progress_current/$progress_total ]($style)";
+          };
+
+          git_status = {
+            style = "fg:black bg:red";
+            ahead = "▲";
+            behind = "▼";
+            conflicted = "±";
+            deleted = "×";
+            diverged = "◊";
+            up_to_date = "√";
+            modified = "‼";
+            staged = "+";
+            renamed = "≡";
+            stashed = "▽";
+            untracked = "?";
+            format = ''[( $all_status$ahead_behind )]($style)'';
+          };
+
+          git_branch = {
+            style = "fg:black bg:green";
+            format = "[ $symbol$branch(:$remote_branch) ]($style)";
+            symbol = "";
+          };
+
+          battery.disabled = true;
+          line_break.disabled = true;
+
+          directory = {
+            truncation_length = 2;
+            style = "bg:blue fg:black";
+            read_only_style = "bg:red fg:black";
+            read_only = " RO ";
+            format = "[$read_only]($read_only_style)[ $path ]($style)";
+          };
+
+          nix_shell = {
+            style = "fg:black bg:yellow";
+            format = "[ $name ]($style)";
+          };
+
+          custom = {
+            jjid = {
+              ignore_timeout = true;
+              description = "current jj revision";
+              when = "jj root";
+              command = "jj log -r@ --no-graph --ignore-working-copy --color never --limit 1 -T 'change_id.shortest(4)'";
+              style = "fg:black bg:green";
+              format = "[ $output ]($style)";
+            };
+
+            jjstat = {
+              ignore_timeout = true;
+              description = "current jj bookmark status";
+              when = "jj root";
+              style = "fg:black bg:red";
+              format = "[( $output )]($style)";
+              command = ''
+                jj log -r@ -n1 --ignore-working-copy --no-graph --color never -T '
+                  separate("",
+                    if(!empty, "∆"),
+                    if(conflict, "${config.programs.starship.settings.git_status.conflicted}"),
+                    if(divergent, "${config.programs.starship.settings.git_status.diverged}"),
+                    if(hidden, "${config.programs.starship.settings.git_status.deleted}"),
+                    if(immutable, "λ"),
+                    if(git_head, "★"),
+                    if(root, "☆")
+                  )
+                '
+              '';
+            };
+          };
+        };
       };
       direnv = {
         enable = true;
