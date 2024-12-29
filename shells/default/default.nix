1403a932f12f2a1f5d3e2797e944e5b7cb568322
daniel <danielxu0307@proton.me>
2024-12-28 16:39:33 -0800

    nvm

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -5,15 +5,14 @@
   ...
 }:
 pkgs.mkShellNoCC {
-  buildInputs = with pkgs;
-    [
-      git
-      git-crypt
-      just
-      nix-output-monitor
-      nixd
-      nvd
-      sops
-    ]
-    ++ lib.optionals pkgs.stdenv.isDarwin [inputs.darwin.packages.darwin-rebuild];
+  buildInputs = with pkgs; [
+    alejandra
+    git
+    git-crypt
+    just
+    nix-output-monitor
+    nixd
+    nvd
+    sops
+  ];
 }
