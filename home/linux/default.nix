56f377e876fa234cd04c5bbee5b24f25c84f4dbf
Daniel Xu <hi@danielx.me>
2026-03-13 01:49:05 -0700

    cursor and icon

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -40,9 +40,20 @@
 
   dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+    size = 24;
+  };
+
   gtk = {
     enable = true;
     colorScheme = "dark";
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
