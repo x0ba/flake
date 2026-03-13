0cfe776d19c6aaac7f43f07c0ea49f3d9d11102d
Daniel Xu <hi@danielx.me>
2026-03-13 01:37:23 -0700

    add dark mode, autostart 1p

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -38,6 +38,15 @@
     TERMINAL = "ghostty";
   };
 
+  dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+  gtk = {
+    enable = true;
+    colorScheme = "dark";
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+  };
+
   programs = {
     ghostty = {
       enable = true;
